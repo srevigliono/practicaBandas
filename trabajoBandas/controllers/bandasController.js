9663fc17dcd35fd8715e2f9faf395f9a542e3752
Santiago Revigliono <srevigliono@udesa.edu.ar>
2024-03-24 16:29:39 -0300

    Trabajé en los puntos 1,2,3 y 4.

--- trabajoBandas/controllers/bandasController.js
+++ trabajoBandas/controllers/bandasController.js
@@ -3,8 +3,40 @@ const bandas = require("../db/index.js");
 const controller = {
     index: function (req, res) {
       return res.render("listaBandas", { title: "Lista de bandas", bandas: bandas });
-    }
-  };
+    },
+    banda: function (req, res) {
+        const idBanda = req.params.idBanda;
+        const result = [];
+    
+        for (let i = 0; i < bandas.length; i++) {
+          if (bandas[i].nombre.toLowerCase() === idBanda.toLowerCase()) {
+            result.push(bandas[i]);
+          }
+        }
+        if (result.length == 0) {
+          return res.send("No se encontraron bandas con ese nombre");
+        }
+        return res.send(result);
+      },
+    generos: function (req, res) {
+
+        const genero = req.params.genero;
+        const result = [];
+
+        for (let i = 0; i < bandas.length; i++) {
+        if (bandas[i].genero.toLowerCase() === genero.toLowerCase()) {
+            result.push(bandas[i]);
+        }
+        }
+        if (result.length == 0) {
+        return res.send("no hay bandas encontrados");
+        }
+        return res.send(result);
+    },
+
+}
+
+
 
 module.exports = controller;
 
--- trabajoBandas/routes/bandas.js
+++ trabajoBandas/routes/bandas.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const bandasController = require("../controllers/bandasController")
 
 router.get("/", bandasController.index);
-  
+router.get("/:idBanda",bandasController.banda);
+router.get("/generos/:genero",bandasController.generos);
+
 module.exports = router;
 
 
